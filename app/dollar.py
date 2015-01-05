0de789b9509984644ac85b8285600896c9e11e12
YuQiu <yu.qiu@centrale-marseille.fr>
2015-01-05 10:54:23 +0100

    mul

--- app/dollar.py
+++ app/dollar.py
@@ -1,1 +1,10 @@
 __author__ = 'YuQiu'
+
+
+class Dollar:
+
+    def __init__(self, amount):
+        self.amount = amount
+
+    def __mul__(self, multiplier):
+        return Dollar(self.amount * multiplier)
--- tests/tests_dollar.py
+++ tests/tests_dollar.py
@@ -7,5 +7,7 @@ class TestDollar(unittest.TestCase):
 
     def test_multiplication(self):
         five = Dollar(5)
-        five.times(2)
-        self.assertEquals(10, five.amount)
+        ten = five * 2
+        self.assertEquals(10, ten.amount)
+        ten = five * 3
+        self.assertEquals(15, ten.amount)
